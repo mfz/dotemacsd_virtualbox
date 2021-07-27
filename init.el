fd6f562dd791291d96199be5ae6851fb464146c4
florian <zink.florian@gmail.com>
2021-07-27 13:10:19 +0000

    added org-roam

--- init.el
+++ init.el
@@ -19,7 +19,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(org-pdftools rainbow-delimiters lsp-mode doom-themes all-the-icons doom-modeline solarized-theme color-theme-solarized yasnippet window-numbering which-key use-package org-ref org-noter counsel company)))
+   '(org-roam org-pdftools rainbow-delimiters lsp-mode doom-themes all-the-icons doom-modeline solarized-theme color-theme-solarized yasnippet window-numbering which-key use-package org-ref org-noter counsel company)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
